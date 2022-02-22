b75f9b5f6d3668663eeecaae8b4dfa564ce498c7
Zachary Clay <75220023+Zach-Clay@users.noreply.github.com>
2022-02-22 08:54:06 -0500

    Update About Section

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -12,7 +12,7 @@ export class AboutSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.aboutString = "My name is Zachary Clay. I am a software engineer from Savannah, GA. I'm currently a junior at Clemson University College of Engineering, Computing, and Applied Sciences. I am working towards a Bachelor of Science in Computer Information Systems. My projected graduation date is May of 2023. To tell you a little bit about myself, I am passionate about software engineering and developing my skills in computer science. I love to challenge myself with projects and new technologies. Some of my hobbies and interests outside of software engineering are playing guitar, hanging out with friends, snowboarding, and playing/watching sports.";
+    this.aboutString = "My name is Zachary Clay. I am a software engineer from Savannah, GA and currently a junior at Clemson University College of Engineering, Computing, and Applied Sciences. I am working towards a Bachelor of Science in Computer Information Systems with a projected graduation date of May 2023. To tell you a little bit about myself, I am passionate about software engineering and developing my skills in computer science. I love to challenge myself with projects and new technologies. Some of my hobbies and interests outside of software engineering are playing guitar, hanging out with friends, snowboarding, and playing/watching sports.";
   }
 
 }
