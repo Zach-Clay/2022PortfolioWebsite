643db701618b5f2fe50fe5d7ce15eb93b8646362
Zach Clay <zachclayc130@gmail.com>
2022-05-07 08:07:32 -0400

    Update About Section

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -5,14 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-section.component.html',
   styleUrls: ['./about-section.component.css']
 })
+
 export class AboutSectionComponent implements OnInit {
-  
-  aboutString = "";
+  aboutString: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
-    this.aboutString = "My name is Zachary Clay. I am a software engineer from Savannah, GA and currently a junior at Clemson University College of Engineering, Computing, and Applied Sciences. I am working towards a Bachelor of Science in Computer Information Systems with a projected graduation date of May 2023. To tell you a little bit about myself, I am passionate about software engineering and developing my skills in computer science. I love to challenge myself with projects and new technologies. Some of my hobbies and interests outside of software engineering are playing guitar, hanging out with friends, snowboarding, and playing/watching sports.";
+    this.aboutString = "My name is Zachary Clay. I am a software engineer from Savannah, GA and currently a senior at Clemson University College of Engineering, Computing, and Applied Sciences. I am working towards a Bachelor of Science in Computer Information Systems with a projected graduation date of May 2023. To tell you a little bit about myself, I am passionate about software engineering and developing my skills in computer science. I love to challenge myself with projects and new technologies. Some of my hobbies and interests outside of software engineering are playing guitar, hanging out with friends, snowboarding, and playing/watching sports.";
   }
 
 }
